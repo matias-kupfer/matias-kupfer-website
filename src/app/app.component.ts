40414464fb0b5864ac84bcde1e3efe5c38463279
mmatiascfgm <matiascfgm@gmail.com>
2020-10-04 19:25:56 +0200

    First commit
    web finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss', './responsive.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'matias-kupfer';
+  data;
+
+  view: any[] = [700, 150];
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+  constructor(public http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    /*this.http.get('https://api.github.com/repos/matiascfgm/yourpage/languages').subscribe((res) => {
+      let total = 0;
+      this.data = [];
+      for (const key in res) {
+        if (key) {
+          total += res[key];
+          this.data.push({name: key, value: res[key]});
+        }
+      }
+      this.data.forEach((value, index) => {
+        this.data[index].value = (this.data[index].value / total) * 100;
+      });
+    });*/
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatChipsModule} from '@angular/material/chips';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import {MatRippleModule} from '@angular/material/core';
+
 
 @NgModule({
   declarations: [
@@ -10,9 +19,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatChipsModule,
+    NgxChartsModule,
+    MatRippleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
